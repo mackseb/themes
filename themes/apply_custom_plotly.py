abb12942bb06390a5e99be0dd5b9b7bbe7013b18
Sebastian Mack <mack.seb@gmail.com>
2021-06-10 17:44:09 +0200

    test

--- themes/apply_custom_plotly.py
+++ themes/apply_custom_plotly.py
@@ -52,7 +52,7 @@ def set_template():
         'hoverlabel': {'align': 'left'},
         'hovermode': 'closest',
         'mapbox': {'style': 'light'},
-        'paper_bgcolor': 'white',
+        'paper_bgcolor': 'black',
         'plot_bgcolor': 'white',
         'polar': {'angularaxis': {'gridcolor': '#EBF0F8', 'linecolor': '#EBF0F8', 'ticks': ''},
                   'bgcolor': 'white',
